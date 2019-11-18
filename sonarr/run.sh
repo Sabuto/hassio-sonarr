6f2813e3c12cee8a88a9f43414258c83715cc212
Robert Dunne <rober_dunne@hotmail.com>
2019-11-18 09:51:17 +0000

    add nginx reformatting to run.sh

--- sonarr/run.sh
+++ sonarr/run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bashio
 WAIT_PIDS=()
 
+bashio::log.info "Setting up frontend...."
+
+sed -i "s/%%PORT%%/8080/g" /etc/nginx/nginx-ingress.conf
+sed -i "s/%%PORT_INGRESS%%/8099/g" /etc/nginx/nginx-ingress.conf
+
+nginx -c /etc/nginx/nginx-ingress.conf &
+WAIT_PIDS+=($!)
+
 bashio::log.info "Starting the Sonarr addon...."
 
 cd /opt/NzbDrone || bashio::exit.nok "setup gone wrong!"
