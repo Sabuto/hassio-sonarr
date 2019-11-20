3c7f0667f5b8f066891369ab20c5fbcb96320271
Robert Dunne <robe_dunne@hotmail.com>
2019-11-20 12:56:56 +0000

    remove first slash from ingress entry

--- sonarr/rootfs/run.sh
+++ sonarr/rootfs/run.sh
@@ -26,6 +26,7 @@ fi
 
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
+ingress_entry={$ingress_entry:1}
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
